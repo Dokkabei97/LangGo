828063d70765a15b90a87ab090b6275e75887173
Dokkabei97 <wkdrn970@naver.com>
2021-10-03 21:38:45 +0900

    [GORM] GORM 테스트용

---
+++ src/domain/skill.go
@@ -0,0 +1,6 @@
+package domain
+
+type Skill struct {
+	Lang string
+	Tier int
+}
--- src/main.go
+++ src/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"domain"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
 func main() {
-	
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("DB 연결에 실패했습니다!")
+	}
+
+	db.AutoMigrate(&domain.Skill{})
+
+	db.Create(&domain.Skill{Lang: "Java", Tier: 3})
+
+	var skill domain.Skill
+	db.First(&skill, 1)
+	db.First(&skill, "lang = ?", "Java")
+
+	db.Model(&skill).Update("Tier", 1)
+
+	db.Delete(&skill, 1)
 }
