621a260cbe88deb25d030a2535411246e5fc5c89
Dokkabei97 <wkdrn970@naver.com>
2021-10-04 04:46:14 +0900

    [Skill] ServiceLogic

--- src/domain/skill.go
+++ src/domain/skill.go
@@ -1,10 +1,38 @@
 package domain
 
-import "gorm.io/gorm"
+import (
+	"config"
+	"gorm.io/gorm"
+)
 
 type Skill struct {
 	gorm.Model        // 기본적으로 id, create_at, updated_at, deleted_at 제공
 	Lang       string // 보유 스킬
 	Tier       uint   // 스킬 숙련도
 	UserID     uint   // 유저 fk
+}
+
+func findByLang(lang string) {
+	// todo 스킬 언어별 검색
+}
+
+func findByTier(tier uint) {
+	// todo 스킬 티어별 검색
+}
+
+func CreateSkill(skill *Skill) (err error) {
+	if err = config.DB.Create(skill).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateSkill(skill *Skill) (err error) {
+	// todo 스킬 업데이트
+	return nil
+}
+
+func DeleteSkill(skill *Skill) (err error) {
+	// todo 스킬 삭제
+	return nil
 }
